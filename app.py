e824a255b7bf0e3de0aaaf4561917db8bbb7bdf0
Jahongir Tech Group <jahongirmutual@gmail.com>
2025-09-13 06:45:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-# oxirgi kelgan qiymatni saqlash uchun global o'zgaruvchi
-last_value = ['']
+# oxirgi kelgan qiymatlarni saqlash
+last_value = []
 
 @app.route("/", methods=["POST", "GET"])
 def index():
     global last_value
     if request.method == "POST":
-        # foydalanuvchidan qiymat qabul qilamiz
-        last_value.append(request.data.decode("utf-8"))
+        data = request.data.decode("utf-8")  # bytes → string
+        last_value.append(data)
         return "OK"
-    else:  # GET
-        return last_value
+    # GET so'rovi uchun JSON qaytarish
+    return jsonify(last_value)
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
